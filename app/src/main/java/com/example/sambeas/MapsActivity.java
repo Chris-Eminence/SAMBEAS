fd6c945a89134155d2978bd7ab44a72d207f061c
chris-eminence <emichris380@gmail.com>
2022-05-09 12:33:11 +0100

    voice record feature added

--- app/src/main/java/com/example/sambeas/MapsActivity.java
+++ app/src/main/java/com/example/sambeas/MapsActivity.java
@@ -4,14 +4,23 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Build;
+import android.os.Environment;
 import android.os.Looper;
 import androidx.core.app.ActivityCompat;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 
 import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AlertDialog;
@@ -30,6 +39,7 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
+import java.io.IOException;
 import java.util.List;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -40,7 +50,23 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     Location mLastLocation;
     Marker mCurrLocationMarker;
     FusedLocationProviderClient mFusedLocationClient;
-    Button sendSms;
+    Button sendSms, stopBtn, playBtn, recordBtn;
+
+    // creating a variable for media recorder object class.
+    private MediaRecorder mRecorder;
+
+    // creating a variable for mediaplayer class
+    private MediaPlayer mPlayer;
+
+    // string variable is created for storing a file name
+    private static String mFileName = null;
+
+    // constant for storing audio permission
+    public static final int REQUEST_AUDIO_PERMISSION_CODE = 1;
+    private int requestCode;
+    private String[] permissions;
+    private int[] grantResults;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +89,40 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 sendingSms();
             }
         });
+
+        recordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // start recording method will
+                // start the recording of audio.
+                startRecording();
+            }
+        });
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // pause Recording method will
+                // pause the recording of audio.
+                pauseRecording();
+
+            }
+        });
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // play audio method will play
+                // the audio which we have recorded
+                playAudio();
+            }
+        });
+//        stopplayTV.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // pause play method will
+//                // pause the play of audio
+//                pausePlaying();
+//            }
+//        });
     }
 
 
@@ -172,6 +232,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
+
             case MY_PERMISSIONS_REQUEST_LOCATION: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
@@ -199,6 +260,167 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 }
             }
         }
+        if (requestCode == REQUEST_AUDIO_PERMISSION_CODE) {
+            if (grantResults.length > 0) {
+                boolean permissionToRecord = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean permissionToStore = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                if (permissionToRecord && permissionToStore) {
+                    Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 
+    //The Recording class method starts here
+
+
+    private void startRecording() {
+        // check permission method is used to check
+        // that the user has granted permission
+        // to record nd store the audio.
+        if (CheckPermissions()) {
+
+            Toast.makeText(this, "Permission granted in place of text colors", Toast.LENGTH_SHORT).show();
+
+            // setbackgroundcolor method will change
+            // the background color of text view.
+//            stopTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+//            startTV.setBackgroundColor(getResources().getColor(R.color.gray));
+//            playTV.setBackgroundColor(getResources().getColor(R.color.gray));
+//            stopplayTV.setBackgroundColor(getResources().getColor(R.color.gray));
+
+            // we are here initializing our filename variable
+            // with the path of the recorded audio file.
+            mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+            mFileName += "/AudioRecording.3gp";
+
+            // below method is used to initialize
+            // the media recorder clss
+            mRecorder = new MediaRecorder();
+
+            // below method is used to set the audio
+            // source which we are using a mic.
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+
+            // below method is used to set
+            // the output format of the audio.
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+
+            // below method is used to set the
+            // audio encoder for our recorded audio.
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+            // below method is used to set the
+            // output file location for our recorded audio
+            mRecorder.setOutputFile(mFileName);
+            try {
+                // below method will prepare
+                // our audio recorder class
+                mRecorder.prepare();
+            } catch (IOException e) {
+                Log.e("TAG", "prepare() failed");
+            }
+            // start method will start
+            // the audio recording.
+            mRecorder.start();
+//            statusTV.setText("Recording Started");
+        } else {
+            // if audio recording permissions are
+            // not granted by user below method will
+            // ask for runtime permission for mic and storage.
+            RequestPermissions();
+        }
+    }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//
+//        // this method is called when user will
+//        // grant the permission for audio recording.
+//        switch (requestCode) {
+//            case REQUEST_AUDIO_PERMISSION_CODE:
+//                if (grantResults.length > 0) {
+//                    boolean permissionToRecord = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+//                    boolean permissionToStore = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+//                    if (permissionToRecord && permissionToStore) {
+//                        Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_LONG).show();
+//                    } else {
+//                        Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_LONG).show();
+//                    }
+//                }
+//                break;
+//        }
+//    }
+
+    public boolean CheckPermissions() {
+        // this method is used to check permission
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void RequestPermissions() {
+        // this method is used to request the
+        // permission for audio recording and storage.
+        ActivityCompat.requestPermissions(MapsActivity.this, new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
+    }
+
+
+    public void playAudio() {
+//        stopTV.setBackgroundColor(getResources().getColor(R.color.purple_500));
+//        startTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+//        playTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+//        stopplayTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+
+        // for playing our recorded audio
+        // we are using media player class.
+        mPlayer = new MediaPlayer();
+        try {
+            // below method is used to set the
+            // data source which will be our file name
+            mPlayer.setDataSource(mFileName);
+
+            // below method will prepare our media player
+            mPlayer.prepare();
+
+            // below method will start our media player.
+            mPlayer.start();
+//            statusTV.setText("Recording Started Playing");
+        } catch (IOException e) {
+            Log.e("TAG", "prepare() failed");
+        }
+    }
+
+    public void pauseRecording() {
+//        stopTV.setBackgroundColor(getResources().getColor(R.color.purple_700));
+//        startTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+//        playTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+//        stopplayTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+
+        // below method will stop
+        // the audio recording.
+        mRecorder.stop();
+
+        // below method will release
+        // the media recorder class.
+        mRecorder.release();
+        mRecorder = null;
+//        statusTV.setText("Recording Stopped");
+    }
+
+    public void pausePlaying() {
+        // this method will release the media player
+        // class and pause the playing of our recorded audio.
+        mPlayer.release();
+        mPlayer = null;
+//        stopTV.setBackgroundColor(getResources().getColor(R.color.purple_700));
+//        startTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+//        playTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+//        stopplayTV.setBackgroundColor(getResources().getColor(R.color.purple_700));
+//        statusTV.setText("Recording Play Stopped");
+    }
 }
+
+
