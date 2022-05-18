f30ae6f7de8cff51ded6fea30d34d5cd9ef21933
chris-eminence <emichris380@gmail.com>
2022-05-18 11:47:29 +0100

    homescreen widget button

---
+++ app/src/main/java/com/example/sambeas/AppWidgetProvider.java
@@ -0,0 +1,26 @@
+package com.example.sambeas;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class AppWidgetProvider extends android.appwidget.AppWidgetProvider {
+
+    //method that is responsible for updating our widget
+
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds){
+            Intent intent = new Intent(context, MapsActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_widget);
+            views.setOnClickPendingIntent(R.id.widget_button, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
